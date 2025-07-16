6cfddbc9538e8811dbdd52d9de823e1bf1e827cc
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-16 10:51:43 -0300

    build(deps): upgrade filament packages to v4.0.0-beta16
    Update filament-related packages and dependencies in `composer.json` and `composer.lock` to align with the latest release. Remove deprecated component configurations to reflect changes in the newer version.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -123,10 +123,6 @@ class AppServiceProvider extends ServiceProvider
                 ->grouped();
         });
 
-        Forms\Components\Placeholder::configureUsing(function (Forms\Components\Placeholder $component) {
-            return $component->columnSpanFull();
-        });
-
         Forms\Components\TextInput::configureUsing(function (Forms\Components\TextInput $component) {
             return $component->minValue(0);
         });
@@ -165,10 +161,6 @@ class AppServiceProvider extends ServiceProvider
 
         Forms\Components\FileUpload::configureUsing(function (Forms\Components\FileUpload $component) {
             return $component->moveFiles();
-        });
-
-        Forms\Components\RichEditor::configureUsing(function (Forms\Components\RichEditor $component) {
-            return $component->disableToolbarButtons(['blockquote']);
         });
 
         Forms\Components\Textarea::configureUsing(function (Forms\Components\Textarea $component) {
